5ac496d9bf36f362f4320bf0749a4f1cc9a1a8c8
Unknown <daniel135790@gmail.com>
2020-03-17 15:25:31 +0200

    Basic layout with side menu and main content

---
+++ src/Layout/Sidebar/Sidebar.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './sidebar.css';
+
+const Sidebar = () => (
+    <div className="sidebar">
+        <div className="item">
+            Home
+        </div>
+    </div>
+);
+
+export default Sidebar;
