bd5c00ca55500db57296bda03c0879731baa3155
Unknown <daniel135790@gmail.com>
2020-03-17 22:50:02 +0200

    Created basic menu with material ui

--- src/Layout/Sidebar/Sidebar.jsx
+++ src/Layout/Sidebar/Sidebar.jsx
@@ -1,12 +1,43 @@
 import React from 'react';
-import './sidebar.css';
+import {Drawer, ThemeProvider} from '@material-ui/core';
+import {List, ListItem, ListItemAvatar, ListItemText} from '@material-ui/core';
+import ChatIcon from '@material-ui/icons/Chat';
+import useStyles from './styles';
+import theme from './theme';
 
-const Sidebar = () => (
-    <div className="sidebar">
-        <div className="item">
-            Home
-        </div>
-    </div>
-);
+const Sidebar = () => {
+    const styles = useStyles();
+
+    return (
+        <ThemeProvider theme={theme}>
+            <Drawer
+                variant="permanent"
+                classes={{
+                paper: styles.paper
+            }}>
+                <List>
+                    <ListItem selected button key={1}>
+                        <ListItemAvatar>
+                            <ChatIcon />
+                        </ListItemAvatar>
+                        <ListItemText primary="Chat" />
+                    </ListItem>
+                    <ListItem button key={2}>
+                        <ListItemAvatar>
+                            <ChatIcon />
+                        </ListItemAvatar>
+                        <ListItemText primary="Chat" />
+                    </ListItem>
+                    <ListItem button key={3}>
+                        <ListItemAvatar>
+                            <ChatIcon />
+                        </ListItemAvatar>
+                        <ListItemText primary="Chat" />
+                    </ListItem>
+                </List>
+            </Drawer>
+        </ThemeProvider>
+    );
+}
 
 export default Sidebar;
---
+++ src/Layout/Sidebar/theme.js
@@ -0,0 +1,38 @@
+import {createMuiTheme} from '@material-ui/core';
+
+const DRAWER_WIDTH = 200;
+
+const theme = createMuiTheme({
+    typography: {
+        fontSize: 12
+    },
+    overrides: {
+        MuiDrawer: {
+            root: {
+                width: DRAWER_WIDTH
+            },
+            paper: {
+                width: DRAWER_WIDTH
+            }
+        },
+        MuiListItem: {
+            root: {
+                "&$selected": {
+                    "backgroundColor": "#12191de3"
+                }
+            },
+            button: {
+                '&:hover': {
+                    backgroundColor: '#32414a'
+                }
+            }
+        },
+        MuiSvgIcon: {
+            root: {
+                color: "#b9b9b9"
+            }
+        }
+    }
+});
+
+export default theme;
