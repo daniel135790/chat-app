fbaf1afdc0460e5bf284e63f5360509d2367ceea
Unknown <daniel135790@gmail.com>
2020-03-18 13:55:38 +0200

    Created pages folder and NotFound page

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import {BrowserRouter as Router, Switch, Route} from "react-router-dom";
 import Sidebar from './Layout/Sidebar';
 import Main from './Layout/Main';
+import * as Pages from './Pages';
 import './App.css';
 
 const App = () => (
@@ -11,7 +12,7 @@ const App = () => (
       <Switch>
         <Route exact path="/" component={Main} />
         <Route path="/chat" component={Main} />
-        <Route path="*" render={() => <div>Not Found</div>} />
+        <Route path="*" component={Pages.NotFound} />
       </Switch>
     </Router>
   </div>
