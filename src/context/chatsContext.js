931d3aa09bb8777dcede15382246afac12c5cc6c
Unknown <daniel135790@gmail.com>
2020-03-25 20:26:06 +0200

    Testing config

--- src/context/chatsContext.js
+++ src/context/chatsContext.js
@@ -1,5 +1,6 @@
 import React, {createContext, useState, useEffect, useCallback} from 'react';
 import {chatService} from '../Services';
+import config from './../config';
 
 export const ChatsContext = createContext();
 
@@ -23,7 +24,7 @@ const ChatsProvider = ({children}) => {
 
     useEffect(() => {
         if (!chatService.validateConnected()) {
-            chatService.connect('wss://ds-chat2-server.herokuapp.com', null, onMessageReceived);
+            chatService.connect(config.SERVER_URL, null, onMessageReceived);
         }
 
         return () => chatService.disconnect();
